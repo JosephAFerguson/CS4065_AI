ce21c472b972644f67f0b66b83bc21b57d989a4d
Brad Greene <bradleytg326@gmail.com>
2024-09-15 13:02:50 -0400

    Update menu prints

--- Assignment 1 - SEARCH/runthis.py
+++ Assignment 1 - SEARCH/runthis.py
@@ -24,45 +24,56 @@ print('Starting Program...\n')
 
 cont = True
 while cont:
-    dem = str(input('Would you like to see the demonstration? Enter Y for Yes or N for No:'))
-    if dem=='Y':
-        choice = int(input('Which search algorithm would you like to run? Enter 1 for Breadth-First, 2 for Depth-First, 3 for Greedy, 4 for A* : '))
+    dem = str(input('Would you like to see the demonstration? (Y/N):'))
+    if dem == 'Y' or dem == 'y':
+        choice = int(input('Which search algorithm would you like to run?\n1. Breadth-First\n2. Depth-First\n3. Greedy\n4. A*\nInput: '))
         if choice==1:
             test_BFS()
         elif choice==2:
             test_DFS()
         elif choice==3:
             test_G()
-        else:
+        elif choice==4:
             test_A()
-    dem = str(input('Would you like to see the performance test? Enter Y for Yes or N for No:'))
-    if dem=='Y':
-        print('Demonstration rejected. Running performance tests . . .\n')
+        else:
+            print("Unexpected reponse")
+            continue
+    elif dem != 'N' and dem != 'n':
+        print("Unexpected reponse")
+        continue
+    perf = str(input('Would you like to see the performance test? (Y/N):'))
+    if perf == 'Y' or perf == 'y':
+        print("\nTotal time for each search for all 20 nodes, 5 times (100 total searches):\n")
         start = time.time()
         for i in range(100):
             Searches.timedBRS(i%20+1, 'Bucharest')
         end = time.time()
-        print(f'The time taken to run Breadth-First search for all 20 nodes, 5 times (100 searches in total) is {(end-start)*1000} ms.\n')
+        print(f'BFS: {(end-start)*1000:5f} ms.')
 
         start = time.time()
         for i in range(100):
             Searches.timedDFS(i%20+1, 'Bucharest')
         end = time.time()
-        print(f'The time taken to run Depth-First search for all 20 nodes, 5 times (100 searches in total) is {(end-start)*1000} ms.\n')
+        print(f'DFS: {(end-start)*1000:5f} ms.')
 
         start = time.time()
         for i in range(100):
             Searches.timedGreedy(i%20+1, 'Bucharest')
         end = time.time()
-        print(f'The time taken to run Greedy search for all 20 nodes, 5 times (100 searches in total) is {(end-start)*1000} ms.\n')
+        print(f'Greedy: {(end-start)*1000:5f} ms.')
 
         start = time.time()
         for i in range(100):
             Searches.timedA_Star(i%20+1, 'Bucharest')
         end = time.time()
-        print(f'The time taken to run A* search for all 20 nodes, 5 times (100 searches in total) is {(end-start)*1000} ms.\n')
+        print(f'A*: {(end-start)*1000:5f} ms.')
+    elif perf != 'N' and perf != 'n':
+        print("Unexpected reponse")
+        continue
 
-
-    cont = str(input("Please enter Y to continue or N to stop:"))
-    if cont =='N':
+    cont = str(input("Would you like to continue (Y/N):"))
+    if cont == 'N' or cont == 'n':
         break
+    elif cont != 'Y' and cont != 'y':
+        print('Unexpected response')
+        continue
